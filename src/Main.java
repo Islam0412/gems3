ccf567eaa4bde7cb92f488f75889e2a27d865788
Islam0412 <islamlivaza2@gmail.com>
2022-08-15 00:17:08 +0600

    lj,fdkty

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,22 @@ public class Main {
         System.out.println("Health Boss = " + boss.getHealth());
         System.out.println("Damage Boss = " + boss.getBossDamage());
         System.out.println("Defence boss from " + boss.getFireProtection());
+
+
+        for (int i = 0; i < createHeroes().length; i++) {
+            System.out.println(createHeroes()[i].getHealth() + createHeroes()[i].getDamage() + createHeroes()[i].getSuperAbility());
+        }
     }
+
+
+    public static HeroCharacteristicsInShort[] createHeroes() {
+        HeroCharacteristicsInShort magic = new HeroCharacteristicsInShort(250, 50);
+        HeroCharacteristicsInShort warrior = new HeroCharacteristicsInShort(220, 35);
+        HeroCharacteristicsInShort defence = new HeroCharacteristicsInShort(210, 15, "arrow");
+        HeroCharacteristicsInShort[] allHeroes = new HeroCharacteristicsInShort[]{magic, warrior, defence};
+        return allHeroes;
+    }
+
 }
+
+
